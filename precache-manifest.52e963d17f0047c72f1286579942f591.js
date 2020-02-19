802c8ff433bc7db4cd6501f338dc6c5d0d889ef7
danielm2048 <danielmimoun98@gmail.com>
2020-02-19 23:34:17 +0200

    Updates

---
+++ precache-manifest.52e963d17f0047c72f1286579942f591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7285f123d774cce0f25ec0fd16837b8e",
+    "url": "/Quote-Machine/index.html"
+  },
+  {
+    "revision": "d85e31e69278360612c7",
+    "url": "/Quote-Machine/static/css/main.cd7eb33d.chunk.css"
+  },
+  {
+    "revision": "dd4fb83f0f7b4c734a23",
+    "url": "/Quote-Machine/static/js/2.2740e1ef.chunk.js"
+  },
+  {
+    "revision": "2e2232e23d1bc626762f7d60ebfe2281",
+    "url": "/Quote-Machine/static/js/2.2740e1ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d85e31e69278360612c7",
+    "url": "/Quote-Machine/static/js/main.85993727.chunk.js"
+  },
+  {
+    "revision": "ab3638d5f2d96315294e",
+    "url": "/Quote-Machine/static/js/runtime-main.9bab01cc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Quote-Machine/precache-manifest.241b4517a56165a02b86f0360cef0613.js"
+  "/Quote-Machine/precache-manifest.52e963d17f0047c72f1286579942f591.js"
 );
 
 self.addEventListener('message', (event) => {
